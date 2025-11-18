68a0453a426ca69f876c1948217305164a5577d9
nazmul8451 <nazmul8451>
2025-11-18 06:35:07 +0600

    add dark mode theme and authentication screen design complete

---
+++ lib/View/Screen/authentication_screen/code_submit.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/gestures.dart' show TapGestureRecognizer;
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+import '../../Theme/theme_provider.dart';
+import '../../Widgets/CustomButton.dart';
+import '../../Widgets/appbar.dart';
+import 'forgot_pass_screen.dart';
+
+class CodeSubmit extends StatefulWidget {
+  const CodeSubmit({super.key});
+
+  static const String name = '/code-submit';
+
+  @override
+  State<CodeSubmit> createState() => _CodeSubmitState();
+}
+
+class _CodeSubmitState extends State<CodeSubmit> {
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController OtpTEController = TextEditingController();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: common_appbar(titleName: 'Forgot Password'),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Code Submit',
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                fontSize: 32.sp,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(height: 20.h),
+            Text(
+              'Enter the 4-Digit code sent to you at\nevent@gmail.com',
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(height: 20.h),
+            PinCodeTextField(
+              backgroundColor: Colors.transparent,
+              length: 4,
+              obscureText: false,
+              animationType: AnimationType.none,
+              keyboardType: TextInputType.number,
+              cursorColor: const Color(0xFFCC18CA),
+
+              textStyle: TextStyle(
+                fontSize: 24.sp,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).textTheme.bodyLarge?.color,
+              ),
+
+              pinTheme: PinTheme(
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(16.r),
+                fieldHeight: 64.h,
+                fieldWidth: 64.w,
+
+                // বর্ডার + আন্ডারলাইন পুরোপুরি লুকিয়ে দাও
+                activeColor: Colors.transparent,
+                inactiveColor: Colors.transparent,
+                selectedColor: Colors.transparent,
+
+                // ফিল কালার (হালকা পার্পল)
+                activeFillColor: const Color(0xFFCC18CA).withOpacity(0.25),
+                selectedFillColor: const Color(0xFFCC18CA).withOpacity(0.35),
+                inactiveFillColor: const Color(0xFFCC18CA).withOpacity(0.15),
+
+                borderWidth: 0,
+              ),
+
+              // ম্যাজিক লাইনগুলো — এই ৩টা না দিলে কখনো ঠিক হবে না
+              enableActiveFill: true,
+              boxShadows: const [],
+              // নিচের shadow চলে যাবে
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+              animationDuration: const Duration(milliseconds: 200),
+              controller: OtpTEController,
+              appContext: context,
+
+              onChanged: (value) {},
+              onCompleted: (value) {
+                print("OTP: $value");
+              },
+            ),
+            SizedBox(height: 20.h),
+            GestureDetector(
+              onTap: () {
+                // Navigator.pushNamed(context, CodeSend.name);
+              },
+              child: CustomButton(buttonName: 'Submit'),
+            ),
+            SizedBox(height: 10.h),
+            Align(
+              alignment: Alignment.centerRight,
+              child: RichText(
+                text: TextSpan(
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  text: "Don't receive code? ",
+                  children: [
+                    TextSpan(
+                      text: 'Resend Again',
+                      style: TextStyle(color: Color(0xFF9810FA)),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap =ontapResendCode,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  void ontapResendCode()
+  {
+    Navigator.pop(context);
+  }
+}
+
+
